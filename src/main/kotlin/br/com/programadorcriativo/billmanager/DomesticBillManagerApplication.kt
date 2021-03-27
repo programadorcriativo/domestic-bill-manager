622c9f492c3403c974c7b536b7441b84f1e3c6e4
programadorcriativo <programadorcriativooficial@gmail.com>
2021-03-27 10:07:52 -0300

    first commit

---
+++ src/main/kotlin/br/com/programadorcriativo/billmanager/DomesticBillManagerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.programadorcriativo.billmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DomesticBillManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DomesticBillManagerApplication>(*args)
+}
